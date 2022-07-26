defaed29350bc4f5001ca99c889f9e623e052473
ingyinmaycgm <ingyinmay@cgm-myanmar.com>
2022-07-26 09:37:57 +0630

    [changes] update backend frame

---
+++ api/src/routes/posts.js
@@ -0,0 +1,14 @@
+const express = require('express');
+const router = express.Router();
+const Post = require('../models/Post')
+
+router.get('/',async(req,res) => {
+  try{
+    const posts = await Post.find().limit(5);
+    res.json(posts)
+  }catch(err){
+    res.json({message:err});
+  }
+});
+
+
